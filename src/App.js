71811cf518ed0464d68194c53a5157f502b25bcc
hatim77100 <najoua-hatim@hotmail.fr>
2023-12-23 21:24:00 +0100

    :tada: initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from "react";
+import Header from "./Header.js";
+import Main from "./Main.js";
+import Loader from "./Loader.js";
+import Error from "./Error.js";
+import StartScreen from "./StartScreen.js";
+import Question from "./Question.js";
 
-function App() {
+const initialState = {
+  questions: [],
+
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return { ...state, status: "active" };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
+export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
+      </Main>
     </div>
   );
 }
-
-export default App;
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <img
+        src="logo512.png"
+        alt="React logo"
+        className="col-start-2 place-items-start "
+      />
+      <h1 className="col-row-2 place-items-center justify-self-start">
+        The React Quiz
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
